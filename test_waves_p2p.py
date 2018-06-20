168e82ca4ae841254bd3f69402573b316b2dfb04
Daniel Newton <djpnewton@gmail.com>
2018-06-20 17:03:12 +1200

    convert to python3

--- test_waves_p2p.py
+++ test_waves_p2p.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python3
+
 import sys
 import socket
 import struct
 import random
 import time
 import binascii
-import requests
 import threading
+
+import requests
+import base58
 
 MAGIC = 305419896
 CONTENT_ID_TX = 0x19
@@ -15,40 +19,13 @@ CONTENT_ID_SCORE = 0x18
 our_score = 0
 our_height = 0
 
-def base58_encode(data):
-    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-    ####convert each digit into unicode
-    buffer = [ord(each[0]) for each in str(data)]
-    digits = [0]; i= 0; j = 0;
-    while (i < len(buffer)):   #loop for as many digits are present
-            j = 0
-            while (j < len(digits)):
-                    digits[j] <<= 8
-                    j += 1
-            digits[0] += buffer[i]
-            carry = 0; j = 0;
-            while(j < len(digits)):
-                    digits[j] += carry
-                    carry = (digits[j]/58) | 0
-                    digits[j] %= 58;
-                    j += 1
-            while(carry):
-                    digits.append(carry%58)
-                    carry = (carry/58) | 0
-            i += 1
-    i = 0;
-    while (buffer[i] == 0 and i < len(buffer) - 1):
-            digits.push(0);
-            i += 1;
-    return "".join([alphabet[each] for each in digits][::-1])
-
 def create_handshake(port):
-    name = "wavesT"
+    name = b"wavesT"
     name_len = len(name) 
     version_major = 0
     version_minor = 13
     version_patch = 2
-    node_name = "utx"
+    node_name = b"utx"
     node_name_len = len(node_name)
     node_nonce = random.randint(0, 10000)
     declared_address = 0x7f000001 #"127.0.0.1"
@@ -56,7 +33,6 @@ def create_handshake(port):
     declared_address_len = 8
     timestamp = int(time.time())
     fmt = ">B%dslllB%dsQlllQ" % (name_len, node_name_len)
-    print struct.calcsize(fmt)
     return struct.pack(fmt, name_len, name,
             version_major, version_minor, version_patch,
             node_name_len, node_name, node_nonce,
@@ -69,12 +45,12 @@ def create_score_message(score):
     return struct.pack(fmt, struct.calcsize(fmt)-1, MAGIC, CONTENT_ID_SCORE, len(payload), payload) 
 
 def decode_handshake(msg):
-    l = ord(msg[0])
-    if l == 6 and msg[1:7] == "wavesT":
+    l = msg[0]
+    if l == 6 and msg[1:7] == b"wavesT":
         msg = msg[7:]
         vmaj, vmin, vpatch = struct.unpack_from(">lll", msg)
         msg = msg[12:]
-        l = ord(msg[0])
+        l = msg[0]
         node_name = msg[1:1+l]
         msg = msg[1+l:]
         nonce, decl_addr_len, decl_addr, decl_addr_port, timestamp = struct.unpack(">QlllQ", msg)
@@ -83,7 +59,7 @@ def decode_handshake(msg):
 def to_hex(data):
     s = ""
     for c in data:
-        s += "%02X," % ord(c)
+        s += "%02X," % c
     return s
 
 def parse_transfer_tx(payload):
@@ -98,7 +74,7 @@ def parse_transfer_tx(payload):
         asset_id_len = 32
         asset_id = payload[offset:offset+asset_id_len]
     offset += asset_id_len
-    fee_asset_flag = ord(payload[offset])
+    fee_asset_flag = payload[offset]
     offset += 1
     fee_asset_id_len = 0
     fee_asset_id = ""
@@ -118,9 +94,9 @@ def parse_transfer_tx(payload):
 def parse_message(socket, msg):
     handshake = decode_handshake(msg)
     if handshake:
-        print "handshake:"
+        print("handshake:")
         for part in handshake:
-            print "", part
+            print("", part)
     else:
         while msg:
             fmt = ">llBl"
@@ -141,47 +117,47 @@ def parse_message(socket, msg):
 
             msg = msg[4 + length:]
 
-            print "message:"
-            print "  length", length
-            print "  magic", magic
-            print "  content_id", "0x%02X" % content_id
-            print "  payload_len", payload_len
-            print "  payload:", to_hex(payload)
+            print("message:")
+            print("  length", length)
+            print("  magic", magic)
+            print("  content_id", "0x%02X" % content_id)
+            print("  payload_len", payload_len)
+            print("  payload:", to_hex(payload))
 
             if magic != MAGIC:
-                print "invalid magic"
+                print("invalid magic")
                 break
 
             if content_id == CONTENT_ID_TX:
                 # transaction!
-                tx_type = ord(payload[0])
-                print "transaction type:", tx_type
+                tx_type = payload[0]
+                print("transaction type:", tx_type)
                 if tx_type == 4:
                     # transfer
                     tx_len, tx_type, sig, tx_type2, pubkey, asset_flag, asset_id, timestamp, amount, fee, address, attachment = parse_transfer_tx(payload)
 
-                    print "  senders pubkey:", base58_encode(pubkey)
-                    print "  addr:", base58_encode(address)
-                    print "  amount:", amount
-                    print "  fee:", fee
-                    print "  asset id:", asset_id
-                    print "  timestamp:", timestamp
-                    print "  attachment:", attachment
+                    print("  senders pubkey:", base58.b58encode(pubkey))
+                    print("  addr:", base58.b58encode(address))
+                    print("  amount:", amount)
+                    print("  fee:", fee)
+                    print("  asset id:", asset_id)
+                    print("  timestamp:", timestamp)
+                    print("  attachment:", attachment)
 
             if content_id == CONTENT_ID_BLOCK:
                 # block
-                print "block:", len(payload)
+                print("block:", len(payload))
 
                 while payload:
                     tx_len, tx_type, sig, tx_type2, pubkey, asset_flag, asset_id, timestamp, amount, fee, address, attachment = parse_transfer_tx(payload)
 
-                    print "  senders pubkey:", base58_encode(pubkey)
-                    print "  addr:", base58_encode(address)
-                    print "  amount:", amount
-                    print "  fee:", fee
-                    print "  asset id:", asset_id
-                    print "  timestamp:", timestamp
-                    print "  attachment:", attachment
+                    print("  senders pubkey:", base58.b58encode(pubkey))
+                    print("  addr:", base58.b58encode(address))
+                    print("  amount:", amount)
+                    print("  fee:", fee)
+                    print("  asset id:", asset_id)
+                    print("  timestamp:", timestamp)
+                    print("  attachment:", attachment)
 
                     payload = payload[tx_len:]
 
@@ -189,14 +165,14 @@ def parse_message(socket, msg):
                 # score
                 global our_score
                 score = int(binascii.hexlify(payload), 16)
-                print "score:", len(payload)
-                print "  value:", score
+                print("score:", len(payload))
+                print("  value:", score)
                 #if score == 0:
                 #    our_score = score
                 #elif score > our_score and our_score != 0:
                 #    # send our score (hopefully they will send us the latest blocks)
                 #    msg = create_score_message(our_score)
-                #    print socket.send(msg)
+                #    print(socket.send(msg))
 
 def block_thread():
     global our_height
@@ -212,18 +188,18 @@ def block_thread():
             our_height = new_height - 1
         while our_height < new_height:
             req_block = our_height + 1
-            print "requesting block:", req_block
+            print("requesting block:", req_block)
             r = requests.get("http://127.0.0.1:6869/blocks/at/%d" % req_block)
             assert(r.status_code == 200)
             txs = r.json()["transactions"]
             for tx in txs:
                 if tx["type"] == 4:
-                    print "  tx:"
-                    print "    recipient:", tx["recipient"]
-                    print "    amount:", tx["amount"]
-                    print "    fee:", tx["fee"]
-                    print "    assetId:", tx["assetId"]
-                    print "    feeAssetId:", tx["feeAssetId"]
+                    print("  tx:")
+                    print("    recipient:", tx["recipient"])
+                    print("    amount:", tx["amount"])
+                    print("    fee:", tx["fee"])
+                    print("    assetId:", tx["assetId"])
+                    print("    feeAssetId:", tx["feeAssetId"])
 
             our_height += 1
 
@@ -253,16 +229,16 @@ def test_p2p():
 
     # send handshake
     handshake = create_handshake(local_port)
-    print to_hex(handshake)
-    print s.send(handshake)
+    print(to_hex(handshake))
+    print(s.send(handshake))
 
     while 1:
         # read reply
         data = s.recv(1024)
         if data:
-            print
-            print len(data)
-            print to_hex(data)
+            print()
+            print(len(data))
+            print(to_hex(data))
             parse_message(s, data)
         else:
             sys.stdout.write(".")
