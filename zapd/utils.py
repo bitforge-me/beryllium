76fca8d8c1753503fc4e32819ac559a3951c0276
Daniel Newton <djpnewton@gmail.com>
2018-11-30 13:55:53 +1300

    fix webhook logging

--- zapd/utils.py
+++ zapd/utils.py
@@ -46,7 +46,7 @@ def create_signed_payment_notification(txid, timestamp, recipient, sender, amoun
 def call_webhook(logger, msg, sig):
     try:
         headers = {"Content-Type": "application/json", "Signature": sig}
-        logger.debug(f"calling '{cfg.webhook_url}' with headers ({json.dumps(headers)}) and body ({body})") 
+        logger.debug(f"calling '{cfg.webhook_url}' with headers ({headers}) and body ({msg})") 
         response = requests.post(cfg.webhook_url, headers=headers, data=msg)
         if response.ok:
             logger.info(f"called {cfg.webhook_url} ok")
