9b40095b2a0b237c14f87f30e88ffed762659a52
Daniel Newton <djpnewton@gmail.com>
2019-03-05 10:23:31 +1300

    increase the block diff detection on testnet, add 'testnet' or 'mainnet' to email subjects

--- ansible/templates/zapd_block_height.sh
+++ ansible/templates/zapd_block_height.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 dest_email={{ ALERT_EMAIL }}
-max_height_diff=2
+max_height_diff={{ 5 if TESTNET else 3 }}
+deploy_type={{ 'testnet' if TESTNET else 'mainnet' }}
 
 remote_block=`curl -s https://{{ REMOTE_WAVES_NODES }}/blocks/height | jq '.["height"]'` 
 local_block=`curl -s localhost:6869/blocks/height | jq '.["height"]'`
@@ -13,8 +14,7 @@ if [ $num -lt 0 ]; then
 	num=$((-$num))
 fi
 if [ $num -ge $max_height_diff ]; then
-	echo "The remote node is at $remote_block while the local node is at $local_block." | mail -s "The nodes are not synced" $dest_email
-else
+	echo "The remote node is at $remote_block while the local node is at $local_block." | mail -s "[$deploy_type] The nodes are not synced" $dest_email
 	exit
 fi
 num=$(( $remote_block - $zapd_scanned_block_height ))
@@ -22,7 +22,6 @@ if [ $num -lt 0 ]; then
 	num=$((-$num))
 fi
 if [ $num -ge $max_height_diff ]; then
-	echo "The remote node is at $remote_block while the zapd scanned height is at $zapd_scanned_block_height." | mail -s "The zapd scanned block height is behind" $dest_email
-else
+	echo "The remote node is at $remote_block while the zapd scanned height is at $zapd_scanned_block_height." | mail -s "[$deploy_type] The zapd scanned block height is behind" $dest_email
 	exit
 fi
