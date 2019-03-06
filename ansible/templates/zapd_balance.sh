57950a948eca552c17f53aaa0142da946d3b7774
Daniel Newton <djpnewton@gmail.com>
2019-03-07 11:11:55 +1300

    add deploy type prefixes to more alert emails

--- ansible/templates/zapd_balance.sh
+++ ansible/templates/zapd_balance.sh
@@ -7,12 +7,13 @@ zap_bal=$(( bal / 100 ))
 min_bal={{ min_zap }}
 max_bal={{ max_zap }}
 dest_email={{ ALERT_EMAIL }}
+deploy_type={{ 'testnet' if TESTNET else 'mainnet' }}
 
 
 if [ $zap_bal -lt $min_bal ]; then
-        echo "balance $zap_bal is less than {{ min_zap }} ZAP" | mail -s "The balance is less than {{ min_zap }} ZAP" $dest_email
+        echo "balance $zap_bal is less than {{ min_zap }} ZAP" | mail -s "[$deploy_type] The balance is less than {{ min_zap }} ZAP" $dest_email
 elif [ $zap_bal -gt $max_bal ]; then
-        echo "balance $zap_bal is greater than {{ max_zap }} ZAP" | mail -s "The balance is greater than {{ max_zap }} ZAP" $dest_email
+        echo "balance $zap_bal is greater than {{ max_zap }} ZAP" | mail -s "[$deploy_type] The balance is greater than {{ max_zap }} ZAP" $dest_email
 else
         exit
 fi
--- ansible/templates/zapd_heartbeat.sh
+++ ansible/templates/zapd_heartbeat.sh
@@ -4,11 +4,12 @@ dest_email={{ ALERT_EMAIL }}
 app_port=5000
 app="zapd"
 host="localhost"
+deploy_type={{ 'testnet' if TESTNET else 'mainnet' }}
 
 curl -s ${host}:${app_port} > /dev/null
 
 if [ $? -ne 0 ]; then
-        echo "ZAPD is not responding on port ${app_port}." | mail -s "ZAPD not responding on port ${app_port}" $dest_email
+        echo "ZAPD is not responding on port ${app_port}." | mail -s "[$deploy_type] ZAPD not responding on port ${app_port}" $dest_email
         exit
 else
         exit
