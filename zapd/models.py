d97d7293852c5fb7d330581b21ba9822701a769e
eoliveros <47799652+eoliveros@users.noreply.github.com>
2020-01-22 15:30:32 +1300

    Added searches for category, proposer, and authorizer (#7)
    * Added searches for category, proposer, and authorizer
    
    * -stop using join against the user table for proposal filters
    -fill user emails and categories in the filter drop downs
    
    Co-authored-by: djpnewton <djpnewton@gmail.com>

--- zapd/models.py
+++ zapd/models.py
@@ -275,6 +275,36 @@ class DateTimeGreaterFilter(FilterGreater, filters.BaseDateTimeFilter):
 class DateSmallerFilter(FilterSmaller, filters.BaseDateFilter):
     pass
 
+class FilterByProposer(BaseSQLAFilter):
+    def apply(self, query, value, alias=None):
+        return query.filter(Proposal.proposer_id == value)
+
+    def operation(self):
+        return u'equals'
+
+    def get_options(self, view):
+        return [(user.id, user.email) for user in User.query.order_by(User.email)]
+
+class FilterByAuthorizer(BaseSQLAFilter):
+    def apply(self, query, value, alias=None):
+        return query.filter(Proposal.authorizer_id == value)
+
+    def operation(self):
+        return u'equals'
+
+    def get_options(self, view):
+        return [(user.id, user.email) for user in User.query.order_by(User.email)]
+
+class FilterByCategory(BaseSQLAFilter):
+    def apply(self, query, value, alias=None):
+        return query.join(Proposal.categories).filter(Category.id == value)
+
+    def operation(self):
+        return u'equals'
+
+    def get_options(self, view):
+        return [(category.id, category.name) for category in Category.query.order_by(Category.name)]
+
 class ProposalModelView(BaseModelView):
     can_create = False
     can_delete = False
@@ -361,7 +391,7 @@ class ProposalModelView(BaseModelView):
     column_labels = {'proposer': 'Proposed by', 'authorizer': 'Authorized by'}
     column_type_formatters = MY_DEFAULT_FORMATTERS
     column_formatters = {'proposer': _format_proposer_column, 'authorizer': _format_proposer_column, 'status': _format_status_column, 'total': _format_total_column}
-    column_filters = [ DateBetweenFilter(Proposal.date, 'Search Date'), DateTimeGreaterFilter(Proposal.date, 'Search Date'), DateSmallerFilter(Proposal.date, 'Search Date'), FilterEqual(Proposal.status, 'Search Status'), FilterNotEqual(Proposal.status, 'Search Status') ]
+    column_filters = [ DateBetweenFilter(Proposal.date, 'Search Date'), DateTimeGreaterFilter(Proposal.date, 'Search Date'), DateSmallerFilter(Proposal.date, 'Search Date'), FilterEqual(Proposal.status, 'Search Status'), FilterNotEqual(Proposal.status, 'Search Status'), FilterByProposer(None, 'Search Proposer'), FilterByAuthorizer(None, 'Search Authorizer'), FilterByCategory(None, 'Search Category') ]
     column_export_list = ('id', 'date', 'proposer', 'categories', 'authorizer', 'reason', 'date_authorized', 'date_expiry', 'status', 'total', 'claimed')
     column_formatters_export = {'total': _format_total_column_export, 'claimed': _format_totalclaimed_column_export}
     form_columns = ['reason', 'categories', 'recipient', 'message', 'amount', 'csvfile']
