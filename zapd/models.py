f2ac8663e5ea898958ce75d0a3650000f8ef02bc
Daniel Newton <djpnewton@gmail.com>
2020-02-26 10:47:21 +1300

    reuse code

--- zapd/models.py
+++ zapd/models.py
@@ -348,14 +348,18 @@ class ProposalModelView(BaseModelView):
             return Markup(html)
         return model.status
 
-    def _format_claimed_column(view, context, model, name):
+    def _format_claimed(view, model):
         if model.status == model.STATE_DECLINED:
-            return Markup('-')
+            return '-'
         total_claimed = 0
         for payment in model.payments:
             if payment.status == payment.STATE_SENT_FUNDS:
                 total_claimed += payment.amount
         total_claimed = decimal.Decimal(total_claimed) / 100
+        return total_claimed
+
+    def _format_claimed_column(view, context, model, name):
+        total_claimed = view._format_claimed(model)
         payments_url = url_for('.payments_view', proposal_id=model.id)
 
         html = '''
@@ -363,7 +367,7 @@ class ProposalModelView(BaseModelView):
         '''.format(payments_url=payments_url, total_claimed=total_claimed)
         return Markup(html)
 
-    def _format_total_column_export(view, context, model, name):
+    def _format_total_column(view, context, model, name):
         if model.status == model.STATE_DECLINED:
             return Markup('-')
         total = 0
@@ -373,23 +377,17 @@ class ProposalModelView(BaseModelView):
         return Markup(total)
 
     def _format_totalclaimed_column_export(view, context, model, name):
-        if model.status == model.STATE_DECLINED:
-            return Markup('-')
-        total_claimed = 0
-        for payment in model.payments:
-            if payment.status == payment.STATE_SENT_FUNDS:
-                total_claimed += payment.amount
-        total_claimed = total_claimed / 100
+        total_claimed = view._format_claimed(model)
         return Markup(total_claimed)
 
     column_default_sort = ('id', True)
     column_list = ('id', 'date', 'proposer', 'categories', 'authorizer', 'reason', 'date_authorized', 'date_expiry', 'status', 'Proposed Total', 'Claimed')
     column_labels = {'proposer': 'Proposed by', 'authorizer': 'Authorized by'}
     column_type_formatters = MY_DEFAULT_FORMATTERS
-    column_formatters = {'proposer': _format_proposer_column, 'authorizer': _format_proposer_column, 'status': _format_status_column, 'Proposed Total': _format_total_column_export, 'Claimed': _format_claimed_column}
+    column_formatters = {'proposer': _format_proposer_column, 'authorizer': _format_proposer_column, 'status': _format_status_column, 'Proposed Total': _format_total_column, 'Claimed': _format_claimed_column}
     column_filters = [ DateBetweenFilter(Proposal.date, 'Search Date'), DateTimeGreaterFilter(Proposal.date, 'Search Date'), DateSmallerFilter(Proposal.date, 'Search Date'), FilterEqual(Proposal.status, 'Search Status', options=get_status_options), FilterNotEqual(Proposal.status, 'Search Status', options=get_status_options), FilterByProposer(None, 'Search Proposer'), FilterByAuthorizer(None, 'Search Authorizer'), FilterByCategory(None, 'Search Category') ]
     column_export_list = ('id', 'date', 'proposer', 'categories', 'authorizer', 'reason', 'date_authorized', 'date_expiry', 'status', 'total', 'claimed')
-    column_formatters_export = {'total': _format_total_column_export, 'claimed': _format_totalclaimed_column_export}
+    column_formatters_export = {'total': _format_total_column, 'claimed': _format_totalclaimed_column_export}
     form_columns = ['reason', 'categories', 'recipient', 'message', 'amount', 'csvfile']
     form_extra_fields = {'recipient': TextField('Recipient'), 'message': TextField('Message'), 'amount': DecimalField('Amount', validators=[validators.Optional()]), 'csvfile': FileField('CSV File')}
 
