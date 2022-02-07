25bbd8c4dcf1d971854bcf11ad33c4124390370a
Alex <aboyle@zap.me>
2022-02-08 12:48:55 +1300

    Fix pylint trailing white spaces error

--- api_endpoint.py
+++ api_endpoint.py
@@ -345,10 +345,10 @@ def user_update_password():
         return bad_request(web_utils.INCORRECT_PASSWORD)
     ### set the new_password:
     if ((len(new_password) <= 7) 
-            or (not any(elem in new_password for elem in '1234567890')) 
-            or (not any(elem in new_password for elem in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')) 
-            or (not any(elem in new_password for elem in 'abcdefghijklmnopqrstuvwxyz')) 
-            or (not any(elem in new_password for elem in '!@#$%^&*()'))):
+    or (not any(elem in new_password for elem in '1234567890')) 
+    or (not any(elem in new_password for elem in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')) 
+    or (not any(elem in new_password for elem in 'abcdefghijklmnopqrstuvwxyz')) 
+    or (not any(elem in new_password for elem in '!@#$%^&*()'))):
         return bad_request(web_utils.WEAK_PASSWORD)
     user.password = encrypt_password(new_password)
     db.session.add(user)
