675e6190d033641b0cd4c2b155b672bfedc209c5
Daniel Newton <djpnewton@gmail.com>
2022-03-30 15:17:58 +1300

    ignore scoped_session in pylint

--- pylint.sh
+++ pylint.sh
@@ -5,4 +5,5 @@ pylint `ls -R|grep .py$|xargs` \
     --load-plugins=pylint_flask_sqlalchemy,pylint_flask \
     --max-parents=10 --max-args=12 --max-branches=18 --max-returns=12 --max-locals=20 --min-similarity-lines=8 \
     --extension-pkg-whitelist=pyblake2,axolotl_curve25519,math \
+    --ignored-classes=scoped_session \
     --ignore=rebalance.py
