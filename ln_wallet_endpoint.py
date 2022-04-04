cacd3a876b9a172433f2bfb6ab8fdfccafcd145f
Erick Oliveros <eoliveros@redratclothing.co.nz>
2022-04-04 14:56:25 +1200

    rename list_peers to rebalance_channel(ln_wallet_enpoint and index). remove the comments in close.html.

--- ln_wallet_endpoint.py
+++ ln_wallet_endpoint.py
@@ -80,9 +80,9 @@ def ln_invoice():
         qrcode_svg = qrcode_svg_create(bolt11, 10)
     return render_template("lightning/invoice.html", bolt11=bolt11, label=label, qrcode_svg=qrcode_svg)
 
-@ln_wallet.route('/list_peers', methods=['GET', 'POST'])
+@ln_wallet.route('/rebalance_channel', methods=['GET', 'POST'])
 @roles_accepted(Role.ROLE_ADMIN)
-def list_peers():
+def rebalance_channel():
     """ Returns a template listing all connected LN peers """
     rpc = LnRpc()
     if request.method == 'POST':
@@ -122,7 +122,7 @@ def list_peers():
         peers[i]["sats_total"] = int(peers[i]["sats_total"])
         peers[i]["can_send"] = int(peers[i]["can_send"])
         peers[i]["can_receive"] = int(peers[i]["can_receive"])
-    return render_template("lightning/list_peers.html", peers=peers)
+    return render_template("lightning/rebalance_channel.html", peers=peers)
 
 @ln_wallet.route('/list_forwards')
 @roles_accepted(Role.ROLE_ADMIN)
