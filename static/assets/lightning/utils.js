1a4995a3fcb5b6dcd74667cebb418e45c0e64738
Daniel Newton <djpnewton@gmail.com>
2022-06-10 15:38:09 +1200

    add eslint, fix js

--- static/assets/lightning/utils.js
+++ static/assets/lightning/utils.js
@@ -1,25 +1,28 @@
+/* exported decodePsbt */
+/* exported formatOutputs */
+
 async function decodePsbt(psbt) {
-    const response = await fetch('decode_psbt?' + new URLSearchParams({psbt: psbt}));
+    const response = await fetch('decode_psbt?' + new URLSearchParams({psbt}));
     if (!response.ok) {
         bootbox.alert({message: `An error has occured decoding the psbt: ${response.status}`});
         return null;
     }
     const data = await response.json();
-    
+
     const fee = data.fee;
-    var outputs = [];
-    for (var i = 0; i < data.tx.vout.length; i++) {
+    const outputs = [];
+    for (let i = 0; i < data.tx.vout.length; i++) {
         const vout = data.tx.vout[i];
         const addr = vout.scriptPubKey.address;
-        var output = [addr, vout.value];
+        const output = [addr, vout.value];
         outputs.push(output);
     }
-    return {'fee': fee, 'outputs': outputs};
+    return {fee, outputs};
 }
 
 function formatOutputs(fee, outputs) {
-    var html = '<ul>';
-    for (var i = 0; i < outputs.length; i++) {
+    let html = '<ul>';
+    for (let i = 0; i < outputs.length; i++) {
         const output = outputs[i];
         html = html + `<li>Address: ${output[0]}, Amount: ${output[1]} BTC</li>`;
     }
