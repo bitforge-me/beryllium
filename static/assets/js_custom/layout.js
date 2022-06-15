2afd61bd05ff59a55306da2b5da3800078b3b2a9
Daniel Newton <djpnewton@gmail.com>
2022-06-15 15:26:21 +1200

    fix CSP on login/logout form

---
+++ static/assets/js_custom/layout.js
@@ -0,0 +1,12 @@
+const loginUrl = document.currentScript.getAttribute('data-login-url');
+const logoutUrl = document.currentScript.getAttribute('data-logout-url');
+
+$(document).ready(function() {
+    $('#btn-login').click(function() {
+        window.location.href = loginUrl;
+    });
+
+    $('#btn-logout').click(function() {
+        window.location.href = logoutUrl;
+    });
+});
