7376484ddc81460b87d25d5e137452f406779fe3
Erick Oliveros <eoliveros@redratclothing.co.nz>
2022-06-20 16:18:28 +1200

    config-generator.sh to generate the lightningd config file and have the entrypoint-shim execute the script.

--- lightningd/entrypoint-shim.sh
+++ lightningd/entrypoint-shim.sh
@@ -7,4 +7,7 @@ if [ -n "$LIGHTNINGD_REPLICA" ]; then
   wallet="$wallet:/root/.lightning_replica/lightningd.sqlite3"
 fi
 
+#generate the config file using the env variable in the container
+./config-generator.sh
+
 ./entrypoint.sh --wallet "$wallet" "$@"
