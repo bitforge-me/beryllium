986d9a3b6522a5774e052b2a1ce945957bbc3d6b
Daniel Newton <djpnewton@gmail.com>
2022-06-20 17:06:08 +1200

    add deploy to beryllium-test

--- ansible/ansible_deploy.sh
+++ ansible/ansible_deploy.sh
@@ -7,6 +7,7 @@ DEPLOY_PRODUCTION=production
 DEPLOY_TYPE=$1
 DEPLOY_LEVEL_BERYLLIUM_ONLY=beryllium_only
 DEPLOY_LEVEL=$2
+DEPLOY_BRANCH=$3
 
 display_usage() { 
     echo -e "\nUsage:
@@ -14,6 +15,8 @@ display_usage() {
     ansible_deploy.sh <DEPLOY_TYPE ($DEPLOY_TEST | $DEPLOY_PRODUCTION)> 
 
     ansible_deploy.sh <DEPLOY_TYPE ($DEPLOY_TEST | $DEPLOY_PRODUCTION)> <DEPLOY_LEVEL ($DEPLOY_LEVEL_BERYLLIUM_ONLY)>
+
+    ansible_deploy.sh <DEPLOY_TYPE ($DEPLOY_TEST | $DEPLOY_PRODUCTION)> <DEPLOY_LEVEL ($DEPLOY_LEVEL_BERYLLIUM_ONLY)> <DEPLOY_BRANCH (any git branch)>
 
         This is a lesser deploy scenario:
 
@@ -59,6 +62,14 @@ else
     FULL_DEPLOY=true
 fi 
 
+# check whether we have a valid deploy branch
+if [ $# -ge 3 ]
+then
+    DEPLOY_BRANCH=$3
+else
+    DEPLOY_BRANCH=master
+fi 
+
 ADMIN_EMAIL=admin@zap.me
 # set deploy variables for production
 DEPLOY_HOST=beryllium.zap.me
@@ -77,6 +88,7 @@ fi
 echo ":: DEPLOYMENT DETAILS ::"
 echo "   - DEPLOY_HOST:     $DEPLOY_HOST"
 echo "   - DEPLOY_LEVEL:    $DEPLOY_LEVEL"
+echo "   - DEPLOY_BRANCH:   $DEPLOY_BRANCH"
 echo "   - TESTNET:         $TESTNET"
 echo "   - ADMIN_EMAIL:     $ADMIN_EMAIL"
 echo "   - DEPLOY_USER:     $DEPLOY_USER"
@@ -89,6 +101,6 @@ then
     # do dangerous stuff
     echo ok lets go!!!
     ansible-playbook --inventory "$DEPLOY_HOST," --user "$DEPLOY_USER" -v \
-        --extra-vars "admin_email=$ADMIN_EMAIL deploy_host=$DEPLOY_HOST full_deploy=$FULL_DEPLOY testnet=$TESTNET DEPLOY_TYPE=$DEPLOY_TYPE" \
+        --extra-vars "admin_email=$ADMIN_EMAIL deploy_host=$DEPLOY_HOST full_deploy=$FULL_DEPLOY testnet=$TESTNET DEPLOY_TYPE=$DEPLOY_TYPE deploy_branch=$DEPLOY_BRANCH" \
         deploy.yml
 fi
