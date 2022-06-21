9b8bc459d4f4ead55ca54657c83f765f5f80eefe
eoliveros <47799652+eoliveros@users.noreply.github.com>
2022-06-21 16:25:44 +1200

    fix lightningd config not receiving lightning environment variables from docker-compose (#36)
    use environment variables to create lightningd config file

--- lightningd/entrypoint-shim.sh
+++ lightningd/entrypoint-shim.sh
@@ -7,4 +7,6 @@ if [ -n "$LIGHTNINGD_REPLICA" ]; then
   wallet="$wallet:/root/.lightning_replica/lightningd.sqlite3"
 fi
 
+jinja2 /config.j2 > "${LIGHTNINGD_DATA}/config"
+
 ./entrypoint.sh --wallet "$wallet" "$@"
