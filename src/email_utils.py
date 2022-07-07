9f8f9a3ffb23479a404e9bc1b1eb22ee31312487
Erick Oliveros <eoliveros@redratclothing.co.nz>
2022-07-08 10:18:39 +1200

    fixing code to remove the pyright disable for reportGeneralTypeIssues in email_utils.py

--- src/email_utils.py
+++ src/email_utils.py
@@ -25,9 +25,10 @@ def _attachment_inline(b64data, mime_type, filename, content_id):
 def send_email(logger: Logger, subject: str, msg: str, recipient: str | None = None, attachment: str | None = None) -> bool:
     if not recipient:
         recipient = app.config["ADMIN_EMAIL"]
-    if app.config["USE_SENDGRID"] is True:
-        return send_email_sendgrid(logger, subject, msg, recipient, attachment) # pyright: ignore [reportGeneralTypeIssues]
-    return send_email_postfix(logger, subject, msg, recipient, attachment) # pyright: ignore [reportGeneralTypeIssues]
+    assert recipient # shouldnt need this because of the above statement setting recipient, but oh well
+    if app.config["USE_SENDGRID"]:
+        return send_email_sendgrid(logger, subject, msg, recipient, attachment)
+    return send_email_postfix(logger, subject, msg, recipient, attachment)
 
 def send_email_sendgrid(logger: Logger, subject: str, msg: str, recipient: str, attachment: str | None = None) -> bool:
     from_email = From(app.config["FROM_EMAIL"], app.config["FROM_NAME"])
