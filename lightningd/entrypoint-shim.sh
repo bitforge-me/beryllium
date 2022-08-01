877242b8e0c6018e43f169a2b78889e1d3f03616
Erick Oliveros <eoliveros@redratclothing.co.nz>
2022-08-01 16:35:55 +1200

    planning using the default plugins directory

--- lightningd/entrypoint-shim.sh
+++ lightningd/entrypoint-shim.sh
@@ -8,5 +8,12 @@ if [ -n "$LIGHTNINGD_REPLICA" ]; then
 fi
 
 jinja2 /config.j2 > "${LIGHTNINGD_DATA}/config"
+if [ -d ${LIGHTNINGD_DATA}/plugins ]
+then
+  cp /opt/lightningd_plugins/* ${LIGHTNINGD_DATA}/plugins/
+else
+  mkdir ${LIGHTNINGD_DATA}/plugins
+  cp /opt/lightningd_plugins/* ${LIGHTNINGD_DATA}/plugins/
+fi
 
 ./entrypoint.sh --wallet "$wallet" "$@"
