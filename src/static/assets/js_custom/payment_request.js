6bd6a1775f5bb6669bf6e3ee73290ce43c95b56e
Daniel Newton <djpnewton@gmail.com>
2022-08-07 21:50:17 +1200

    make the payment request flow more clear

--- src/static/assets/js_custom/payment_request.js
+++ src/static/assets/js_custom/payment_request.js
@@ -11,4 +11,11 @@ $(document).ready(function() {
             }
         }
     }
+
+    $('#close').click(function() {
+        // 'window.close()' will not work unless JS created the window
+        // so we do it this way to allow us to close this window
+        open(location, '_self').close();
+        return false;
+    });
 });
