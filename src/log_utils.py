2b6eaa8d9d61e23c0e78284d8697870f60dceaa0
Erick Oliveros <eoliveros@redratclothing.co.nz>
2022-08-12 11:00:07 +1200

    setting level for geventwebsocket.handler to WARNING and placing it in log_utils module to be called.

--- src/log_utils.py
+++ src/log_utils.py
@@ -6,6 +6,11 @@ def log_socketio_version(logger):
     logger.info('python-engineio version: %s', version('python-engineio'))
 
 def setup_logging(logger, level):
+    if level < logging.WARNING:
+        # geventwebsocket.handler logger pollutes our log with http requests at the INFO level
+        # we raise the logging level for that module because we can see the http request in our
+        # proxy server logs and want to make the app logs easier to parse
+        logging.getLogger('geventwebsocket.handler').setLevel(logging.WARNING)
     # clear loggers set by any imported modules
     logging.getLogger().handlers.clear()
     # setup logging
