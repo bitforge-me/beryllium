81f6c120e614b03ca0daffa1c07dc16f5d0fa204
Erick Oliveros <eoliveros@redratclothing.co.nz>
2022-08-17 12:11:18 +1200

    configure group for telegraf by making changes to TELEGRAF_NAME_DB_USER_PASS and rename it to include TELEGRAF_NAME_GROUP_DB_USER_PASS

--- telegraf/entrypoint-shim.sh
+++ telegraf/entrypoint-shim.sh
@@ -2,10 +2,10 @@
 set -e
 set -a # export all variables
 
-# split $TELEGRAF_NAME_DB_USER_PASS using | as the field seperator
-IFS="|" read TELEGRAF_NAME TELEGRAF_DB TELEGRAF_USER TELEGRAF_PASS <<< "$TELEGRAF_NAME_DB_USER_PASS"
+# split $TELEGRAF_NAME_GROUP_DB_USER_PASS using | as the field seperator
+IFS="|" read TELEGRAF_NAME TELEGRAF_GROUP TELEGRAF_DB TELEGRAF_USER TELEGRAF_PASS <<< "$TELEGRAF_NAME_GROUP_DB_USER_PASS"
 # generate the telegraf.conf with jinja2
-echo using name $TELEGRAF_NAME, db $TELEGRAF_DB, user $TELEGRAF_USER, pass \*\*\* and url $TELEGRAF_INFLUXDB_URL to create \'telegraf.conf\'
+echo using name $TELEGRAF_NAME, group $TELEGRAF_GROUP, db $TELEGRAF_DB, user $TELEGRAF_USER, pass \*\*\* and url $TELEGRAF_INFLUXDB_URL to create \'telegraf.conf\'
 jinja2 /tmp/telegraf.conf.j2 > "/etc/telegraf/telegraf.conf"
 
 # run the upstream entrypoint.sh
