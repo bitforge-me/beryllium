e7b844f853868097af2c62748e9b8b8b349152fc
Daniel Newton <djpnewton@gmail.com>
2022-09-08 10:55:48 +1200

    remove `process_email_alerts()` task (replaced by monitoring endpoint)

--- src/tasks.py
+++ src/tasks.py
@@ -69,19 +69,6 @@ def flash_and_clear_tasks(category: str):
 # Periodic task functions, !assume we have a flask app context!
 #
 
-def process_email_alerts():
-    data = dasset.account_balances()
-    if not data:
-        logger.error('failed to get dasset account balances')
-        return
-    for balance in data:
-        if balance.symbol == 'NZD':
-            if balance.available < app.config["MIN_AVAILABLE_NZD_BALANCE"]:
-                balance_format = assets.asset_dec_to_str(balance.symbol, balance.available)
-                subject = f"Available {balance.symbol} Balance below the minimum threshold"
-                msg = f"Available {balance.symbol} Balance needs to be replenished in the dasset account.<br/><br/>Available {balance.symbol} balance is: ${balance_format}"
-                email_utils.email_notification_alert(subject, msg, app.config["ADMIN_EMAIL"])
-
 def process_depwith_and_broker_orders():
     logger.info('process deposits..')
     depwith.fiat_deposits_update(db.session)
@@ -96,11 +83,12 @@ def process_depwith_and_broker_orders():
     logger.info('process broker orders..')
     broker.broker_orders_update(db.session)
 
-def process_btc_tx_index():
+
+def _process_btc_tx_index():
     logger.info('process btc tx index..')
     wallet.btc_transactions_index()
 
-def process_dasset_cache():
+def _process_dasset_cache():
     # logger.info('process dasset cache..')
     dasset.order_book_refresh_cache(10)
     dasset.markets_refresh_cache(10)
@@ -114,6 +102,43 @@ def update_withdrawal(asset: str, token: str):
         depwith.crypto_withdrawal_update(db.session, token)
     else:
         depwith.fiat_withdrawal_update(db.session, token)
+
+def ln_rebalance_channels(oscid: str, iscid: str, amount: int):
+    category = 'ln_rebalance_channels'
+    info_str = 'Rebalancing {0} -> {1} with {2} sats'.format(oscid, iscid, amount)
+    task_uid = utils.generate_key()
+    store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_INPROGRESS, info_str))
+    try:
+        email_utils.send_email('Channel Rebalance Successful', info_str)
+        store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_SUCCESS, info_str))
+    except Exception as e:
+        logger.error('ln_rebalance_channels error: %s', e)
+        info_str = 'error: {0}'.format(e)
+        store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_ERROR, info_str))
+
+def send_email_task(subject: str, msg: str, recipient: str | None = None, attachment: str | None = None):
+    if not recipient:
+        recipient = app.config["ADMIN_EMAIL"]
+    assert recipient
+    if app.config["USE_SENDGRID"]:
+        return email_utils.send_email_sendgrid(logger, subject, msg, recipient, attachment)
+    return email_utils.send_email_postfix(logger, subject, msg, recipient, attachment)
+
+def ln_pay_to_invoice(bolt11: str):
+    category = 'ln_pay_to_invoice'
+    task_uid = utils.generate_key()
+    info_str = 'Paid Invoice: {0}'.format(utils.shorten(bolt11))
+    store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_INPROGRESS, info_str))
+    try:
+        LnRpc().pay(bolt11)
+        email_utils.send_email('Paid Invoice', info_str)
+        store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_SUCCESS, info_str))
+    except Exception as e:
+        logger.error('%s error: %s', category, e)
+        info_str = 'error: {0}'.format(e)
+        store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_ERROR, info_str))
+    return info_str
+
 #
 # Non-terminating task functions
 #
@@ -157,48 +182,12 @@ def _process_ln_invoices_loop():
             logger.error('wait_any_invoice error: %s', e)
             gevent.sleep(5, False)
 
-def ln_rebalance_channels(oscid: str, iscid: str, amount: int):
-    category = 'ln_rebalance_channels'
-    info_str = 'Rebalancing {0} -> {1} with {2} sats'.format(oscid, iscid, amount)
-    task_uid = utils.generate_key()
-    store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_INPROGRESS, info_str))
-    try:
-        email_utils.send_email('Channel Rebalance Successful', info_str)
-        store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_SUCCESS, info_str))
-    except Exception as e:
-        logger.error('ln_rebalance_channels error: %s', e)
-        info_str = 'error: {0}'.format(e)
-        store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_ERROR, info_str))
-
-def send_email_task(subject: str, msg: str, recipient: str | None = None, attachment: str | None = None):
-    if not recipient:
-        recipient = app.config["ADMIN_EMAIL"]
-    assert recipient
-    if app.config["USE_SENDGRID"]:
-        return email_utils.send_email_sendgrid(logger, subject, msg, recipient, attachment)
-    return email_utils.send_email_postfix(logger, subject, msg, recipient, attachment)
-
-def ln_pay_to_invoice(bolt11: str):
-    category = 'ln_pay_to_invoice'
-    task_uid = utils.generate_key()
-    info_str = 'Paid Invoice: {0}'.format(utils.shorten(bolt11))
-    store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_INPROGRESS, info_str))
-    try:
-        LnRpc().pay(bolt11)
-        email_utils.send_email('Paid Invoice', info_str)
-        store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_SUCCESS, info_str))
-    except Exception as e:
-        logger.error('%s error: %s', category, e)
-        info_str = 'error: {0}'.format(e)
-        store_task_info(category, task_uid, TaskInfo(TaskInfo.STATUS_ERROR, info_str))
-    return info_str
 #
 # Init tasks
 #
 
 task_manager = TaskManager()
-task_manager.repeated('email alerts', process_email_alerts, 30)
 task_manager.repeated('deposits, withdrawals, orders', process_depwith_and_broker_orders, 5)
-task_manager.repeated('btc tx index', process_btc_tx_index, 60)
-task_manager.repeated('dasset cache', process_dasset_cache, 0)
+task_manager.repeated('btc tx index', _process_btc_tx_index, 60)
+task_manager.repeated('dasset cache', _process_dasset_cache, 0)
 task_manager.non_terminating('ln invoices', _process_ln_invoices_loop)
